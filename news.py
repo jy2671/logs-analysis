30f4eb9db1b56d7af065eb3f3fd55bf9501fd736
Jenny Troilo <jy2671@att.com>
2019-02-21 15:10:17 -0500

    second commit

--- news.py
+++ news.py
@@ -15,7 +15,8 @@ print
 print "Results for question 1 - What are the most popular three articles of all time?"
 print
 for result1 in results1:
-	print " ", "\"", result1[0], "\"  - ", result1[2], " views"
+	#print " ", "\"", result1[0], "\"", " - ", result1[2], "views"
+	print " ", ('"%s"' % result1[0]), " - ", result1[2], "views"
 print
 
 
@@ -28,7 +29,7 @@ print
 print "Results for question 2 - Who are the most popular article authors of all time?"
 print
 for result2 in results2:
-	print " ", result2[0],  " - ", result2[1], " views"
+	print " ", result2[0],  " - ", result2[1], "views"
 print
 
 cursor.execute("select to_char(lt.time, 'Month dd, yyyy') as date, round(((l.sum404/(lt.total*1.00))*100),1) as num from log_total lt, log_404 l where lt.time = l.time and ((l.sum404/(lt.total*1.00))*100)>1;")
@@ -39,7 +40,7 @@ print
 print "Results for question 3 - On which days did more than 1% of requests lead to errors?"
 print
 for result3 in results3:
-	print " ", result3[0], " - ", result3[1], " errors" 
+	print " ", result3[0], " - ", "{0:.1f}%".format(result3[1]), "errors" 
 print
 
 conn.close
