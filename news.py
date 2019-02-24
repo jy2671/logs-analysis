a1e633cbfec6145ca1d46cf89af580bbdc51d090
Jenny Troilo <jy2671@att.com>
2019-02-24 14:20:32 -0500

    updated code quality

--- news.py
+++ news.py
@@ -1,46 +1,109 @@
+#!/usr/bin/env python
+
 import psycopg2
 
-conn = psycopg2.connect("dbname=news")
 
-cursor = conn.cursor()
-cursor.execute("select a.title, a.slug, count(*) as num from log l, articles a where a.slug = substring(l.path, 10, 34) group by (a.slug, a.title) order by num desc limit 3;")
-
-results1 = cursor.fetchall()
-
-#print "Row data:"
-#print results
-
-#loop over each row
-print
-print "Results for question 1 - What are the most popular three articles of all time?"
-print
-for result1 in results1:
-	#print " ", "\"", result1[0], "\"", " - ", result1[2], "views"
-	print " ", ('"%s"' % result1[0]), " - ", result1[2], "views"
-print
+def db_connnect():
+    # create and return a database connection and cursor
+    conn = psycopg2.connect("dbname=news")
+    cursor = conn.cursor()
+    return cursor, conn
 
 
-cursor.execute("select au.name, count(*) as num from log l, articles a, authors au where a.slug = substring(l.path, 10, 34) and au.id = a.author group by (au.name) order by num desc;")
+def db_disconnect():
+    # disconnect connection
+    cursor, conn = db_connnect()
+    cursor.close()
+    del cursor
+    conn.close()
 
-results2 = cursor.fetchall()
 
-#loop over each row
-print
-print "Results for question 2 - Who are the most popular article authors of all time?"
-print
-for result2 in results2:
-	print " ", result2[0],  " - ", result2[1], "views"
-print
+def execute_query(query):
+    # execute_query returns the results of an SQL query
+    cursor, conn = db_connnect()
 
-cursor.execute("select to_char(lt.time, 'Month dd, yyyy') as date, round(((l.sum404/(lt.total*1.00))*100),1) as num from log_total lt, log_404 l where lt.time = l.time and ((l.sum404/(lt.total*1.00))*100)>1;")
+    try:
+        cursor.execute(query)
+        results = cursor.fetchall()
+        return results
+    except Exception as error:
+        print('error execute_query "{}", error: {}'.format(query, error))
+        return None
+    finally:
+        db_disconnect()
 
-results3 = cursor.fetchall()
 
-print
-print "Results for question 3 - On which days did more than 1% of requests lead to errors?"
-print
-for result3 in results3:
-	print " ", result3[0], " - ", "{0:.1f}%".format(result3[1]), "errors" 
-print
+def print_top_articles():
+    # Print out the top 3 articles of all time.
+    query = ("""
+        SELECT a.title, a.slug, count(*) as num
+        FROM log l, articles a
+        WHERE a.slug = substring(l.path, 10, 34)
+        GROUP BY (a.slug, a.title)
+        ORDER BY num desc limit 3;
+        """)
 
-conn.close
+    results = execute_query(query)
+
+    # loop over each row
+    print
+    print("Results for question 1 - "
+          "What are the most popular three articles of all time?")
+    print
+    for result in results:
+        print(' "{}" - {} views'.format(result[0], result[2]))
+    print
+
+
+def print_top_authors():
+    # Print out a list of authors ranked by article views.
+
+    query = ("""
+        SELECT au.name, count(*) as num
+        FROM log l, articles a, authors au
+        WHERE a.slug = substring(l.path, 10, 34) AND au.id = a.author
+        GROUP BY (au.name)
+        ORDER BY num desc;
+        """)
+
+    results = execute_query(query)
+
+    # loop over each row
+    print
+    print("Results for question 2 - "
+          "Who are the most popular article authors of all time?")
+    print
+    for result in results:
+        print result[0],  " - ", result[1], "views"
+    print
+
+
+def print_errors_over_one():
+    ''' print out the error report.
+
+    This function prints out the days and that day's error percentage
+    where more than 1% of logged access requests were errors
+    '''
+    query = ("""
+        SELECT to_char(lt.time, 'FMMonth dd, yyyy') as date,
+        round(((l.sum404/(lt.total*1.00))*100),1) as num
+        FROM log_total lt, log_404 l
+        WHERE lt.time = l.time AND ((l.sum404/(lt.total*1.00))*100)>1;
+        """)
+
+    results = execute_query(query)
+
+    # loop over each row
+    print
+    print("Results for question 3 - "
+          "On which days did more than 1% of requests lead to errors?")
+    print
+    for result in results:
+        print('{} - {}% errors'.format(result[0], result[1]))
+    print
+
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
